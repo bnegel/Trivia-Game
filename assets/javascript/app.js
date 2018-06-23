fca60df436fd4534a2f7dc865e05a5ef68233f7c
Barbara Egel <barbaraegel2012@u.northwestern.edu>
2018-06-23 15:55:37 -0500

    Saturday 3:55

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+
+$("#startBtn").on ("click", function() {
+  ;
+
+
+//Onclick of START button to change page, show questions, start timer
+//
+
+//display 30=second timer and start countown on startBtn click
+
+//
+
+//Consider maybe making the first choice always the right answer for the programming but then 
+//randomizing the placement of the answers afterward. (like for Lumen MGAs).
+
+}
+
+})
