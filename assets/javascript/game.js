41752232ae574e53b2bea4e2c4ed5bb4fd8f6a83
Barbara Egel <barbaraegel2012@u.northwestern.edu>
2018-06-27 22:20:02 -0500

    Wednesday night to turn in

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,22 @@
 var questions = [
     
-   question: "Daughter of the poet, Lord Byron, she published the first algorithm intended for a machine.",
+{question: "Daughter of the poet, Lord Byron, she published the first algorithm intended for a machine.",
     answers: [
         "Elizabeth, Lady Walsington",
         "Eugenia Byron",
         "Ada Lovelace",
         "Mary Shelley"
     ],
-    rightAnswer: 2,
+    rightAnswer: 2},
   
-    question: "Co-designer of the C-10 programming language for UNIVAC, she had been in the US only six years before starting Cornell University as a math major. A side project of hers still in use today is the 'Jewish Holiday Algorithm' for calendar programs.",
+   {question: "Co-designer of the C-10 programming language for UNIVAC, she had been in the US only six years before starting Cornell University as a math major. A side project of hers still in use today is the 'Jewish Holiday Algorithm' for calendar programs.",
     answers: [
       "Liliana Gordon",
       "Ida Rhodes",
       "Gertrude Berg",
       "Molly Bloom"
     ],
-    rightAnswer: 1,
+    rightAnswer: 1},
 
   {question: "Featured in the movie <i>Hidden Figures<i>, this 'human computer' prepared for the advent of machine calculation by teaching herself FORTRAN. She later headed the programming section of the Analysis and Computation Division at Langley Research Center.",
     answers: [
@@ -25,32 +25,28 @@ var questions = [
       "Katherine Johnson",
       "Eloise McCarter"
     ],
-    rightAnswer = 0},
+    rightAnswer: 0},
 
-question: "This Naval officer conceived of the idea of a computer language based on English words. Her work inspired the development of the COBAL programming language.",
+{question: "This Naval officer conceived of the idea of a computer language based on English words. Her work inspired the development of the COBAL programming language.",
     answers: [
       "Admiral Alene Duerk",
       "Admiral Kathryn Janeway",
       "Commander Daphne Blundell",
       "Rear Admiral Grace Murray Hopper"
     ],
-    rightAnswer = 3
-  ,
-  question: "This woman started working at IBM in 1958, mostly on systems architecture and implementation of operating systems and natural language processors; however, she is best known for having her name on a landmark 2013 Supreme Court case.",
+    rightAnswer: 3},
+
+  {question: "This woman started working at IBM in 1958, mostly on systems architecture and implementation of operating systems and natural language processors; however, she is best known for having her name on a landmark 2013 Supreme Court case.",
       answers: [
         "Susan Obergefell",
         "Edith Windsor",
         "Marilyn Hacker",
         "Julia Undeco"
       ],
-      rightAnswer = 1
+      rightAnswer: 1}
     ]
 
-var answers = [
-
-
-    
-]
+var answersChosen = []
 
 
 $(document).ready(function() {
@@ -65,14 +61,15 @@ var startGame = function() {
 };
 var displayQuestions = function() {
 for(var i = 0; i < questions.length; i++) {
-    var currentQuestion = questions[0];
+    var currentQuestion = questions[i];
     var questionBox = $("<div>");
     var questionStem = $("<p>");
     questionStem.text(currentQuestion.question);
    
     displayAnswers(currentQuestion.answers);
     questionBox.append(questionStem);
-    var answerBox =  displayAnswers(currentQuestion.answers);
+    var answerBox =  $("<div>");
+    answerBox = displayAnswers(currentQuestion.answers);
     questionBox.append(answerBox);
     $("#quiz-body").append(questionBox);
 }
@@ -81,11 +78,17 @@ var displayAnswers = function(answerChoices) {
     var answerBox = $("<div>");
     for(var j =0; j < 4; j++) {
         var currentAnswer = answerChoices[j];
-        var answerChoice = $('<input type="radio">' + currentAnswer + "<input>");
+        var answerChoice = $('<input type="radio">' + currentAnswer + "</input>");
       answerChoice.val(currentAnswer)
         answerBox.append(answerChoice);
     }
     return answerBox
+}
+
+var rightAnswer = function() {
+    if()
+}
+
 }
 startGame();
 
